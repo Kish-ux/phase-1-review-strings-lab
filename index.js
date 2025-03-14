144eafdd90fb293b5bd307b5845e5515254fc957
yvonne <yvonnimooo@gmail.com>
2025-03-14 22:10:42 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'welcome to Flatbook', ${currentUser}!;
+const excitesWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${currentUser.slice(0, 1)}!';
